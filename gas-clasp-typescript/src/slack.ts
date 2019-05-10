62c2f127747145dff09e6956beed9d9bfb3b8fc5
Kazuhiro Sera <seratch@gmail.com>
2019-05-10 16:38:46 +0900

    Update GAS examples

--- gas-clasp-typescript/src/slack.ts
+++ gas-clasp-typescript/src/slack.ts
@@ -9,28 +9,76 @@ import * as R from 'seratch-slack-types/web-api';
 const apiToken = PropertiesService.getScriptProperties().getProperty("SLACK_API_TOKEN");
 
 // https://api.slack.com/methods/api.test
-function apiTest() {
+function apiTest(): R.ApiTestResponse {
   const httpResponse = UrlFetchApp.fetch(
     url('api.test'),
     options<A.APITestArguments>(apiToken, {
       'foo': 'bar'
     }));
-  const response = as(httpResponse) as R.ApiTestResponse;
+  const response = as<R.ApiTestResponse>(httpResponse);
   print(response);
+  return response;
 }
 
 // https://api.slack.com/methods/chat.postMessage
-function chatPostMessage(text: string) {
+function chatPostMessage(text: string): R.ChatPostMessageResponse {
   const httpResponse = UrlFetchApp.fetch(
     url('chat.postMessage'),
     options<A.ChatPostMessageArguments>(apiToken, {
       channel: '#random',
-      text: 'Hi!'
+      text: text || 'Hi!'
     }));
   const response = as<R.ChatPostMessageResponse>(httpResponse);
   print(response);
   console.log(`Posted: ${json(response.message)}`);
+  return response;
 }
+
+// https://api.slack.com/methods/channels.list
+function channelsList(): R.ChannelsListResponse {
+  const response = as<R.ChannelsListResponse>(UrlFetchApp.fetch(
+    url('channels.list'),
+    options<A.ChannelsListArguments>(apiToken, {
+      exclude_archived: true,
+      exclude_members: true
+    })
+  ));
+  // `response.channels` can be a large array
+  // print(response);
+  if (response.error) console.error(`Got an error from Slack API: ${response.error}`)
+  return response;
+}
+
+// https://api.slack.com/methods/conversations.history
+function conversationsHistory() {
+  const channels = channelsList().channels;
+  // #find method is not allowed on GAS
+  const channelId: string = channels.reduce((prev, current) => {
+    if (prev) return prev;
+    else if (current.name == 'random') return current;
+    else return null;
+  }, null).id;
+  console.log(`#random: ${channelId}`);
+
+  const httpResponse = UrlFetchApp.fetch(
+    url('conversations.history'),
+    options<A.ConversationsHistoryArguments>(apiToken, {
+      channel: channelId
+    })
+  );
+  const response = as<R.ConversationsHistoryResponse>(httpResponse);
+  // `response.messages` can be a large array
+  // print(response);
+  response.messages.forEach(message => {
+    const messagePart = `${message.text} by @${message.username} ts:${message.ts}`;
+    const reactions = message.reactions
+      ? message.reactions.map((r) => r.name + " * " + r.count)
+      : [];
+    const reactionsPart = reactions.length > 0 ? ` (reactions: ${reactions} )` : '';
+    console.log(`${messagePart}${reactionsPart}`)
+  });
+}
+
 
 // ---------------------------
 // Common Functions
@@ -48,12 +96,14 @@ import { WebAPICallOptions } from '@slack/web-api/dist/WebClient';
 function options<A extends WebAPICallOptions>(
   token: string,
   payload: A): GoogleAppsScript.URL_Fetch.URLFetchRequestOptions {
-  return {
+  const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
     method: 'post',
-    contentType: 'application/json; charset=utf-8',
+    contentType: 'application/x-www-form-urlencoded',
     headers: { 'Authorization': `Bearer ${token}` },
-    payload: JSON.stringify(payload),
+    payload: payload,
   };
+  console.log(options);
+  return options;
 }
 
 function as<T>(response: GoogleAppsScript.URL_Fetch.HTTPResponse): T {
